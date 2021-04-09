bd2a467451607bdd943aa13cdbc194ee1c39175a
noroot <you@example.com>
2021-04-09 13:22:02 +0200

    final v.1

--- main.sh
+++ main.sh
@@ -1,37 +1,81 @@
 #!/bin/bash
+#Inicializar variables
+no_input="No se ha introducido"
+user=$no_input; script=$no_input; comando=$no_input; resp=$no_input; IP=$no_input;
+#---------------------------------------------------------------------------------------------------------------------
+#-------------------######  ##  ##  ##  ##   ####   ######   ####   ##  ##  ######   ####  ---------------------------
+#-------------------##      ##  ##  ### ##  ##  ##    ##    ##  ##  ### ##  ##      ##     ---------------------------
+#-------------------####    ##  ##  ## ###  ##        ##    ##  ##  ## ###  ####     ####  ---------------------------
+#-------------------##      ##  ##  ##  ##  ##  ##    ##    ##  ##  ##  ##  ##          ## ---------------------------
+#-------------------##       ####   ##  ##   ####   ######   ####   ##  ##  ######   ####  ---------------------------
+#---------------------------------------------------------------------------------------------------------------------
+
+#---------------- MENU DE AYUDA ----------------
+help_menu()
+{
+    echo "---------------------- Menu de ayuda ----------------------"
+    echo "-u [usuario]          ---> Para determinar el usuario (admin por defecto)"
+    echo "-i [IP]               ---> Para poner la IP (obligatorio)"
+    echo "-s [ruta_script]      ---> Para poner el script"
+    echo "-c [comando]          ---> Comando para ejecutar en el router (el comando entre '' o \"\") "
+    echo "-y ['yes'|'Yes'|'y']  ---> Para que no pida confirmacion para reiniciar el router de fabrica antes de cargar un script"
+    exit 1
+}
+#---------------- MOSTRAR ARGUMENTOS ----------------
+mostrar_args(){
+    echo "-----------------------------------------------------------"
+    echo "El usuario es: $user"
+    echo "La IP es: $IP"
+    echo "El script es: $script"
+    echo "El comando es: $comando"
+}
+#---------------- EJECUTAR SCRIPT ----------------
+ejecutar_script(){
+    if [[ $resp == $no_input ]]; then
+        read -p "¿Deseas restablecer de fabrica el router y cargar el script (yes/No)? " resp
+    fi
+    if [[ $resp == "yes" || $resp == "Yes"  || $resp == "y"  ]]; then
+        echo "Restableciendo de fabrica y Ejecutando script"
+        scp $script $user@$IP:configuracion.rsc > /dev/null
+        ssh $user@$IP "system reset-configuration no-defaults=yes run-after-reset=configuracion.rsc"
+    else
+        echo "Ejecutando script"
+        scp $script $user@$IP:configuracion.rsc > /dev/null
+        ssh $user@$IP "import configuracion.rsc"
+    fi
+}
+#---------------- EJECUTAR COMANDO ----------------
+ejecutar_comando(){ ssh $user@$IP $comando; }
+
+#---------------------------------------------------------------------------------------------------------------------
+#--------------------------------- COMIENZO DEL SCRIPT ---------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
 
 #Leer variables
-{
-while IFS= read -r "lineNum"
+while getopts u:i:s:c:y: flag
 do
-    variables=${lineNum}
-done
-} <<< $@
-
-#Inicializar variables
-init="No se ha introducido"
-user=$init
-IP=$init
-script=$init
-comando=$init
-let i=0
-
-#Guardar argumentos en variables
-for VAR in $variables
-do
-    if   [[ $user == 1 ]]; then user=$VAR
-    elif [[ $IP == 1 ]]; then IP=$VAR
-    elif [[ $script == 1 ]]; then script=$VAR
-    elif [[ $comando == 1 ]]; then comando=$VAR
-    fi
-    if   [[ $VAR == "-u" ]]; then user=1
-    elif [[ $VAR == "-i" ]]; then IP=1
-    elif [[ $VAR == "-s" ]]; then script=1
-    elif [[ $VAR == "-c" ]]; then comando=1
-    fi
+    case "${flag}" in
+        u)  user=${OPTARG};;
+        i)  IP=${OPTARG};;
+        s)  script=${OPTARG};;
+        c)  comando=${OPTARG};;
+        y)  resp=${OPTARG};;
+    esac
 done
 
-echo $user
-echo $IP
-echo $script
-echo $comando
+mostrar_args
+
+if [[ $IP = $no_input ]]; then
+    echo "-----------------------------------------------------------"
+    echo "Debes introducir una direccion IP"
+    echo
+    help_menu
+fi
+
+if [[ $script != $no_input ]]; then
+    ejecutar_script
+fi
+if [[ $comando != $no_input ]]; then
+    echo "Ejecutando comando"
+    ejecutar_comando
+fi
--- prueba.sh
+++ prueba.sh
@@ -1,37 +1,81 @@
 #!/bin/bash
-
 #Inicializar variables
-init="No se ha introducido"
-user=$init; IP=$init ;script=$init; comando=$init
-let i=0
+no_input="No se ha introducido"
+user=$no_input; script=$no_input; comando=$no_input; resp=$no_input; IP=$no_input;
+#---------------------------------------------------------------------------------------------------------------------
+#-------------------######  ##  ##  ##  ##   ####   ######   ####   ##  ##  ######   ####  ---------------------------
+#-------------------##      ##  ##  ### ##  ##  ##    ##    ##  ##  ### ##  ##      ##     ---------------------------
+#-------------------####    ##  ##  ## ###  ##        ##    ##  ##  ## ###  ####     ####  ---------------------------
+#-------------------##      ##  ##  ##  ##  ##  ##    ##    ##  ##  ##  ##  ##          ## ---------------------------
+#-------------------##       ####   ##  ##   ####   ######   ####   ##  ##  ######   ####  ---------------------------
+#---------------------------------------------------------------------------------------------------------------------
 
-#Menu de ayuda
+#---------------- MENU DE AYUDA ----------------
 help_menu()
 {
-    echo "Menu de ayuda"
+    echo "---------------------- Menu de ayuda ----------------------"
     echo "-u [usuario]          ---> Para determinar el usuario (admin por defecto)"
     echo "-i [IP]               ---> Para poner la IP (obligatorio)"
     echo "-s [ruta_script]      ---> Para poner el script"
     echo "-c [comando]          ---> Comando para ejecutar en el router (el comando entre '' o \"\") "
+    echo "-y ['yes'|'Yes'|'y']  ---> Para que no pida confirmacion para reiniciar el router de fabrica antes de cargar un script"
     exit 1
 }
-valido_ip=`echo $@ | grep '\-i'`
-if [[ $@ = "" || -z $valido_ip ]]; then
-    help_menu
-fi
+#---------------- MOSTRAR ARGUMENTOS ----------------
+mostrar_args(){
+    echo "-----------------------------------------------------------"
+    echo "El usuario es: $user"
+    echo "La IP es: $IP"
+    echo "El script es: $script"
+    echo "El comando es: $comando"
+}
+#---------------- EJECUTAR SCRIPT ----------------
+ejecutar_script(){
+    if [[ $resp == $no_input ]]; then
+        read -p "¿Deseas restablecer de fabrica el router y cargar el script (yes/No)? " resp
+    fi
+    if [[ $resp == "yes" || $resp == "Yes"  || $resp == "y"  ]]; then
+        echo "Restableciendo de fabrica y Ejecutando script"
+        scp $script $user@$IP:configuracion.rsc > /dev/null
+        ssh $user@$IP "system reset-configuration no-defaults=yes run-after-reset=configuracion.rsc"
+    else
+        echo "Ejecutando script"
+        scp $script $user@$IP:configuracion.rsc > /dev/null
+        ssh $user@$IP "import configuracion.rsc"
+    fi
+}
+#---------------- EJECUTAR COMANDO ----------------
+ejecutar_comando(){ ssh $user@$IP $comando; }
+
+#---------------------------------------------------------------------------------------------------------------------
+#--------------------------------- COMIENZO DEL SCRIPT ---------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
+
 #Leer variables
-while getopts u:i:s:c: flag
+while getopts u:i:s:c:y: flag
 do
     case "${flag}" in
-        u) user=${OPTARG};;
-        i) IP=${OPTARG};;
-        s) script=${OPTARG};;
-        c) comando=${OPTARG};;
-
+        u)  user=${OPTARG};;
+        i)  IP=${OPTARG};;
+        s)  script=${OPTARG};;
+        c)  comando=${OPTARG};;
+        y)  resp=${OPTARG};;
     esac
 done
 
-echo $user
-echo $IP
-echo $script
-echo $comando
+mostrar_args
+
+if [[ $IP = $no_input ]]; then
+    echo "-----------------------------------------------------------"
+    echo "Debes introducir una direccion IP"
+    echo
+    help_menu
+fi
+
+if [[ $script != $no_input ]]; then
+    ejecutar_script
+fi
+if [[ $comando != $no_input ]]; then
+    echo "Ejecutando comando"
+    ejecutar_comando
+fi
